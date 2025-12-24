6fb230af025e8ef16c580a8613d7d9902436bfeb
lgthvinh <everwellmax@gmail.com>
2025-12-24 16:18:19 +0700

    refactor: clean up unused imports and commented code in LoginScreen

--- app/src/main/java/com/avis/app/ptalk/ui/screen/auth/LoginScreen.kt
+++ app/src/main/java/com/avis/app/ptalk/ui/screen/auth/LoginScreen.kt
@@ -16,8 +16,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -25,10 +23,6 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -36,11 +30,9 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun LoginScreen(navController: NavController) {
@@ -48,7 +40,6 @@ fun LoginScreen(navController: NavController) {
         modifier = Modifier
             .fillMaxSize()
             .background(Color(0xFFE0E0E0))
-        ,
     ) {
         Box(
             modifier = Modifier
@@ -156,118 +147,5 @@ fun LoginScreen(navController: NavController) {
                 )
             }
         }
-
-//        Box(
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .padding(horizontal = 18.dp),
-//            shape = RoundedCornerShape(10.dp),
-//            colors = CardDefaults.cardColors(containerColor = Color.White),
-//            elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
-//        ) {
-//            Column(
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .padding(horizontal = 16.dp, vertical = 18.dp)
-//            ) {
-//
-//                Spacer(modifier = Modifier.height(8.dp))
-//                OutlinedTextField(
-//                    value = "",
-//                    onValueChange = { },
-//                    modifier = Modifier.fillMaxWidth(),
-//                    placeholder = { Text("Email/Phone") },
-//                    singleLine = true,
-//                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-//                    shape = RoundedCornerShape(22.dp),
-//                    colors = TextFieldDefaults.colors(
-//                        focusedContainerColor = Color(0xFFEDEDED),
-//                        unfocusedContainerColor = Color(0xFFEDEDED),
-//                        focusedIndicatorColor = Color.Transparent,
-//                        unfocusedIndicatorColor = Color.Transparent
-//                    )
-//                )
-//
-//                Spacer(Modifier.height(14.dp))
-//
-//                Text(
-//                    text = "Mật khẩu",
-//                    style = MaterialTheme.typography.titleSmall.copy(fontWeight = FontWeight.Bold)
-//                )
-//                Spacer(Modifier.height(8.dp))
-//                OutlinedTextField(
-//                    value = "",
-//                    onValueChange = { },
-//                    modifier = Modifier.fillMaxWidth(),
-//                    placeholder = { Text("Nhập mật khẩu") },
-//                    singleLine = true,
-//                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-//                    trailingIcon = {
-//                        IconButton(onClick = { }) {
-//                            Icon(
-//                                imageVector = Icons.Filled.Visibility,
-//                                contentDescription = "Show password"
-//                            )
-//                        }
-//                    },
-//                    shape = RoundedCornerShape(22.dp),
-//                    colors = TextFieldDefaults.colors(
-//                        focusedContainerColor = Color(0xFFEDEDED),
-//                        unfocusedContainerColor = Color(0xFFEDEDED),
-//                        focusedIndicatorColor = Color.Transparent,
-//                        unfocusedIndicatorColor = Color.Transparent
-//                    )
-//                )
-//
-//                Spacer(Modifier.height(16.dp))
-//
-//                Button(
-//                    onClick = { },
-//                    enabled = true,
-//                    modifier = Modifier
-//                        .align(Alignment.CenterHorizontally)
-//                        .fillMaxWidth(0.55f),
-//                    contentPadding = PaddingValues(vertical = 12.dp),
-//                    colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF2F80ED)),
-//                    shape = RoundedCornerShape(14.dp)
-//                ) {
-////                    if (state.isLoading) {
-////                        CircularProgressIndicator(
-////                            modifier = Modifier.height(18.dp),
-////                            strokeWidth = 2.dp,
-////                            color = Color.White
-////                        )
-////                        Spacer(Modifier.height(0.dp))
-////                    } else {
-////                        Text("Đăng nhập", color = Color.White, fontWeight = FontWeight.SemiBold)
-////                    }
-//                    Text("Đăng nhập", color = Color.White, fontWeight = FontWeight.SemiBold)
-//                }
-//
-//                Spacer(Modifier.height(12.dp))
-//
-//                Spacer(Modifier.height(12.dp))
-//
-//
-//                Spacer(Modifier.height(6.dp))
-//
-//                Text(
-//                    text = "Quên mật khẩu",
-//                    style = MaterialTheme.typography.bodySmall.copy(
-//                        color = Color.Red,
-//                        fontWeight = FontWeight.SemiBold
-//                    ),
-//                    modifier = Modifier.clickable { }
-//                )
-//            }
-//        }
-//        Spacer(Modifier.height(28.dp))
     }
-}
-
-@Composable
-@Preview
-fun LoginScreenPreview() {
-    val navController = rememberNavController()
-    LoginScreen(navController)
 }
