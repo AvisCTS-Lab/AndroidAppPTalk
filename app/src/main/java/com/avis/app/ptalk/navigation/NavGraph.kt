8de91bc83d12591273ddc0c8c541eee1941f6dd0
lgthvinh <everwellmax@gmail.com>
2025-12-24 18:26:49 +0700

    add navigation transition animations to NavHost
    - add fade and slide animations for enterTransition, exitTransition, popEnterTransition, and popExitTransition in NavGraph

--- app/src/main/java/com/avis/app/ptalk/navigation/NavGraph.kt
+++ app/src/main/java/com/avis/app/ptalk/navigation/NavGraph.kt
@@ -1,5 +1,11 @@
 package com.avis.app.ptalk.navigation
 
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.core.EaseIn
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -13,7 +19,47 @@ fun AppNavGraph(navController: NavHostController, modifier: Modifier = Modifier)
     NavHost(
         navController = navController,
         startDestination = Route.LOGIN,
-        modifier = modifier
+        modifier = modifier,
+        enterTransition = {
+            fadeIn(
+                animationSpec = tween(
+                    300, easing = LinearEasing
+                )
+            ) + slideIntoContainer(
+                animationSpec = tween(300, easing = EaseIn),
+                towards = AnimatedContentTransitionScope.SlideDirection.Start
+            )
+        },
+        exitTransition = {
+            fadeOut(
+                animationSpec = tween(
+                    300, easing = LinearEasing
+                )
+            ) + slideOutOfContainer(
+                animationSpec = tween(300, easing = EaseIn),
+                towards = AnimatedContentTransitionScope.SlideDirection.End
+            )
+        },
+        popEnterTransition = {
+            fadeIn(
+                animationSpec = tween(
+                    300, easing = LinearEasing
+                )
+            ) + slideIntoContainer(
+                animationSpec = tween(300, easing = EaseIn),
+                towards = AnimatedContentTransitionScope.SlideDirection.End
+            )
+        },
+        popExitTransition = {
+            fadeOut(
+                animationSpec = tween(
+                    300, easing = LinearEasing
+                )
+            ) + slideOutOfContainer(
+                animationSpec = tween(300, easing = EaseIn),
+                towards = AnimatedContentTransitionScope.SlideDirection.Start
+            )
+        }
     ) {
         composable(Route.LOGIN) { LoginScreen(navController) }
         composable(Route.SIGNUP) { SignupScreen(navController) }
