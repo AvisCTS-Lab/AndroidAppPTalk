28dc1a6876359402a3b684307ac63d69f3495660
lgthvinh <everwellmax@gmail.com>
2025-12-26 14:47:39 +0700

    update UI layout in LoginScreen and clean up imports in SignupScreen
    - change Box height modifier in LoginScreen to use fractional max height instead of fixed dp
    - remove unused imports (TextFieldDefaults, Color) in SignupScreen

--- app/src/main/java/com/avis/app/ptalk/ui/screen/auth/SignupScreen.kt
+++ app/src/main/java/com/avis/app/ptalk/ui/screen/auth/SignupScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -36,7 +35,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
