33baecdea21a81abeac4d2cc835b2f1db36d4003
lgthvinh <everwellmax@gmail.com>
2025-12-26 16:27:43 +0700

    disable dynamic colors in AndroidPTalkTheme and remove unused imports
    - set dynamicColor default value to false in AndroidPTalkTheme
    - remove unused imports (Activity, Typography, toArgb) in Theme.kt

--- app/src/main/java/com/avis/app/ptalk/ui/theme/Theme.kt
+++ app/src/main/java/com/avis/app/ptalk/ui/theme/Theme.kt
@@ -1,5 +1,5 @@
 package com.avis.app.ptalk.ui.theme
-import android.app.Activity
+
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -7,11 +7,9 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 
 private val lightScheme = lightColorScheme(
@@ -258,7 +256,7 @@ val unspecified_scheme = ColorFamily(
 fun AndroidPTalkTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false, // disable dynamic color
     content: @Composable() () -> Unit
 ) {
   val colorScheme = when {
